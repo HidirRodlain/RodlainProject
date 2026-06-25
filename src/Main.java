60b735a729046e881354c8371c6f6fc9881793d7
MyFirstProject <hidir9346@gmail.com>
2026-06-25 15:06:18 +0300

    Perfect army switch-case by memory

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,114 @@
-import javax.imageio.plugins.tiff.TIFFImageReadParam;
-import javax.naming.PartialResultException;
+
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] match = new int[5];
-        int totalscore = 0;
-        System.out.println("Введите голы за матч: ");
-        for (int i = 0; i < match.length; i ++) {
-            System.out.println("Матч " + (i + 1) + ": ");
-            match[i] = scan.nextInt();
-            totalscore = totalscore + match[0];
+
+
+
+        int money = 1200;
+        int brontehnika = 0;
+        int flot = 0;
+        int army = 125000;
+        System.out.println("Выберите что хотите изменить в армии: \n1.Увеличить количество регулярной армии!\n2.Увеличить количество бронетранспортеров БТР БМП ТАНК\n3.Увеличить флот пополнить боевые катера и корабли");
+        System.out.println("Выберите: ");
+        int choice = scan.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Вы увеличили количество армии + 20.000 солдат!\nНа содеражании армии снимаеться -500");
+                money = money - 500;
+                army = army + 20000;
+                System.out.println("Обшая численость воурженых сил: " + army);
+                System.out.println("Состояния военного бюджета: " + money);
+                break;
+            case 2:
+                System.out.println("ВПК перешло на производство бронетехники, ряды поплнены на 100 машин!\nНа производства -700");
+                brontehnika = brontehnika + 100;
+                money = money - 500;
+                System.out.println("Количество бронетехники: " + brontehnika);
+                System.out.println("Состояние военного бюджета: " + money);
+                break;
+            case 3:
+                System.out.println("Вы увеличи воено морской флот, количество кораблей поплнены на 50 штук!\nНа производство -900");
+                flot = flot + 50;
+                System.out.println("Количество военных кораблей: " + flot);
+                money = money - 900;
+                System.out.println("Состояние военного бюджета: " + money);
         }
-        int max = match[0];
-        int min = match[0];
-        int last = match[match.length -1];
-        for (int i = match.length -1; i > 0; i--) {
-            match[i] = match[i - 1];
-        }
-        System.out.println("После сдвига: ");
-        for (int i = 0; i < match.length; i++) {
-            System.out.println("Матч " + (i + 1) + ": " + match[i]);
-            if (max < match[i]) {
-                max = match[i];
+        if (money >= 1500) {
+            System.out.println("Экономика отчина! ");
+        } else {
+            if (money >= 700) {
+                System.out.println("Экономика стабильна");
+            } else {
+                System.out.println("Экономика в кризисе! ");
             }
-            if (min > match[i]) {
-                min = match[i];
-            }
+
 
         }
-        System.out.println("Лучший матч: " + max);
-        System.out.println("Худчший матч: " + min);
-        System.out.println("Обшее количество глов: " + totalscore);
-
-
     }
+}
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
