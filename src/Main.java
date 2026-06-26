2edbddc22efe19f4ff6f56c909d899f6b050bdd5
MyFirstProject <hidir9346@gmail.com>
2026-06-26 05:06:01 +0300

    Created tactical drone simulator with double Scanner fix

--- src/Main.java
+++ src/Main.java
@@ -3,86 +3,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
+
         Scanner scan = new Scanner(System.in);
-
-
-
-        int money = 1200;
-        int brontehnika = 0;
-        int flot = 0;
-        int army = 125000;
-        System.out.println("Выберите что хотите изменить в армии: \n1.Увеличить количество регулярной армии!\n2.Увеличить количество бронетранспортеров БТР БМП ТАНК\n3.Увеличить флот пополнить боевые катера и корабли");
-        System.out.println("Выберите: ");
-        int choice = scan.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("Вы увеличили количество армии + 20.000 солдат!\nНа содеражании армии снимаеться -500");
-                money = money - 500;
-                army = army + 20000;
-                System.out.println("Обшая численость воурженых сил: " + army);
-                System.out.println("Состояния военного бюджета: " + money);
+        System.out.println("Ударные дроны: \n1.Герань.\n2.Шахед.\n3.Ланцет");
+        String choiceDrone = scan.nextLine();
+        System.out.println("Вы выбрали: " + choiceDrone);
+        switch (choiceDrone) {
+            case "Герань":
+                System.out.println("Выберите по каким пунктам врага будете бить: \n1.Логистика.\n2.Дислокации\n3.Склады БК");
+                int choiceplace = scan.nextInt();
+                System.out.println("Отлично! нажмите кнопку <пуск>");
+                String start = scan.nextLine();
+                start = scan.nextLine();
+                if (start.equals("пуск")) {
+                    System.out.println("До запуска: \n--3--\n--2--\n--1--\nЗапуск!!!");
+                }
                 break;
-            case 2:
-                System.out.println("ВПК перешло на производство бронетехники, ряды поплнены на 100 машин!\nНа производства -700");
-                brontehnika = brontehnika + 100;
-                money = money - 500;
-                System.out.println("Количество бронетехники: " + brontehnika);
-                System.out.println("Состояние военного бюджета: " + money);
+            case "Шахед":
+                System.out.println("Выберите по каким пунктам врага будете бить: \n1.Порт.\n2.Суда.\n3.Танкер");
+                int choiceplace2 = scan.nextInt();
+                System.out.println("Отлично! нажмите кнопку <пуск>");
+                String start2 = scan.nextLine();
+                start2 = scan.nextLine();
+                if (start2.equals("пуск")) {
+                    System.out.println("До запуска: \n--3--\n--2--\n--1--\nЗапуск!!!");
+                }
                 break;
-            case 3:
-                System.out.println("Вы увеличи воено морской флот, количество кораблей поплнены на 50 штук!\nНа производство -900");
-                flot = flot + 50;
-                System.out.println("Количество военных кораблей: " + flot);
-                money = money - 900;
-                System.out.println("Состояние военного бюджета: " + money);
-        }
-        if (money >= 1500) {
-            System.out.println("Экономика отчина! ");
-        } else {
-            if (money >= 700) {
-                System.out.println("Экономика стабильна");
-            } else {
-                System.out.println("Экономика в кризисе! ");
-            }
-
-
+            case "Ланцет":
+                System.out.println("Выберите по каким пунктам врага будете бить: \n1.Завод дронов.\n2.Нефти.\n3.Электростанциям");
+                int choiceplace3 = scan.nextInt();
+                System.out.println("Отлично! нажмите кнопку <пуск> ");
+                String start3 = scan.nextLine();
+                start3 = scan.nextLine();
+                if (start3.equals("пуск")) {
+                    System.out.println("До запуска: \n--3--\n--2--\n--1--\nЗапуск!!!");
+                }
+                break;
+            default:
+                System.out.println("Вы ввели что то не то!");
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
